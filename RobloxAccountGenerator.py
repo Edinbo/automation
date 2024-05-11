3ed7bde190711f5e8b0598b009d48c27e79cf5b3
Enes ERKEN <121829291+Edinbo@users.noreply.github.com>
2024-05-11 11:58:58 +0200

    Create RobloxAccountGenerator.py

---
+++ RobloxAccountGenerator.py
@@ -0,0 +1,91 @@
+import os
+import random
+from rich.console import Console
+from rich.prompt import IntPrompt
+from playwright.sync_api import sync_playwright
+
+console = Console()
+
+def generate_nickname():
+        name = "snakegiggle"
+        return name + str(random.randint(10000, 999999))
+
+
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    console.rule('[bold red]Roblox Account Generator[/]')
+
+
+def registration():
+    status = console.status('Generating...', spinner='line')
+    status.start()
+
+    with sync_playwright() as pw:
+        nickname = generate_nickname()
+        password = 'Sanane1!'
+        browser = pw.firefox.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        page.set_viewport_size({"width": 640, "height": 480})
+        page.route('https://apis.roblox.com/universal-app-configuration/v1/behaviors/cookie-policy/content',
+                   lambda route: route.abort())  # Blocks cookies banner
+        page.goto('https://www.roblox.com')
+        page.locator('//*[@id="MonthDropdown"]').select_option('January')
+        page.locator('//*[@id="DayDropdown"]').select_option('01')
+        page.locator('//*[@id="YearDropdown"]').select_option('1999')
+
+        while True:
+            # Checks username valid
+            with page.expect_response('https://auth.roblox.com/v1/usernames/validate') as username_validate:
+                page.locator('//*[@id="signup-username"]').fill(nickname)
+            if username_validate.value.json().get('code') == 0:
+                break
+            else:
+                nickname = generate_nickname()
+
+        page.locator('//*[@id="signup-password"]').fill(password)
+        page.locator('//*[@id="MaleButton"]').click()
+
+        with page.expect_response('https://auth.roblox.com/v2/signup') as signup_validate:
+            page.locator('//*[@id="signup-button"]').click(timeout=0)
+        if signup_validate.value.status == 429:
+            browser.close()
+            status.stop()
+            console.log(
+                '[bold red]Error:[/] too many requests! Try to change your IP (you can use VPN, preferably paid)')
+            console.print('[bold]Press enter to continue...[/]')
+            console.input()
+            return None
+        else:
+            page.wait_for_url('https://www.roblox.com/home?nu=true', timeout=0)
+
+        with open('cookies.txt', 'a') as file:
+            cookies = context.cookies()
+            roblosecurity_cookie = next((cookie for cookie in cookies if cookie['name'] == '.ROBLOSECURITY'),
+                                        None)
+            if roblosecurity_cookie:
+                roblos_data_value = roblosecurity_cookie['value']
+                file.write(f'{roblos_data_value}\n')
+
+        with open('accounts.txt', 'a') as file:
+            file.write(f'{nickname}:{password}\n')
+        console.log('[bold green]Account generated successfully[/]', ':white_check_mark:')
+        browser.close()
+        status.stop()
+        return None
+
+
+def main():
+    try:
+        clear()
+        amount = IntPrompt.ask('Enter how many accounts to generate')
+        clear()
+        for _ in range(amount):
+            registration()
+    except KeyboardInterrupt:
+        exit()
+
+
+if __name__ == '__main__':
+    main()
